db6f5c42f136327fdb637adb78db059bf50ed996
elonj <elonjacobowitz@gmail.com>
2024-12-09 13:30:57 +0200

    check int2number

--- src/Ex1.java
+++ src/Ex1.java
@@ -23,31 +23,60 @@ public class Ex1 {
             return ans;
         }
         int indexb = num.lastIndexOf('b');
-        if (indexb == -1 || indexb == num.length() - 1 || indexb == 0) {
+        if (!num.contains("b") || indexb == num.length() - 1 || indexb == 0) {
             return ans;
         }
-        int number = Integer.parseInt( num.substring(0, indexb));
-        char baseChar = num.substring(indexb + 1).charAt(0);
-        int baseInt;
-        if (baseChar >= '2' && baseChar <= '9'){
-            baseInt = baseChar - 48;
+        String[] parts = num.split("b");
+        if (parts.length != 2) {
+            return ans;
+        }
+        String numberpart = parts[0];
+        String basepart = parts[1];
+        int s = numberpart.length();
+        char[] charbase = basepart.toCharArray();
+        char[] eachnumber = numberpart.toCharArray();
+        int number;
+        String temp = "";
+        for (int i = 0; i < s; i++) {
+            if (eachnumber[i] >= '0' && eachnumber[i] <= '9') {
+                if (eachnumber[i] < charbase[0]) {
+                    number = eachnumber[i] - '0';
+                    temp = temp + number;
+                } else return ans;
+            } else if (eachnumber[i] >= 'A' && eachnumber[i] <= 'G') {
+                if (eachnumber[i] < charbase[0]) {
+                    number = eachnumber[i] - 'A' + 10;
+                    temp = temp + number;
+                } else return ans;
+            }
         }
 
-        else if(baseChar >= 'A' && baseChar <= 'G') {
-            baseInt = baseChar - 55;
+        int base;
+        if (basepart.length() == 1) {
+            char basechar = Character.toUpperCase(basepart.charAt(0));
+            if (basechar >= '2' && basechar <= '9') {
+                base = basechar - '0';
+            } else if (basechar >= 'A' && basechar <= 'G') {
+                base = basechar - 'A' + 10;
+            } else {
+                return ans;
+            }
+        } else {
+            return ans;
         }
-        else {
-            return -1;
+        int intnumber = Integer.parseInt(temp);
+        String answer = "";
+        while (intnumber >= base) {
+            int x = intnumber / base; // 67
+            int remainder = intnumber % base;
+            answer = remainder + answer;
+            intnumber = x;
         }
-        String temp = "";
-        while( number >= baseInt){
-            int x = number/baseInt; // 67
-            int remainder = number % baseInt;
-            temp = remainder + temp;
-            number = x;
-        }
-        temp = number + temp;
-        return Integer.parseInt(temp);
+        answer = intnumber + answer;
+        return Integer.parseInt(answer);
+
+
+
     }
     /**
      * This static function checks if the given String (g) is in a valid "number" format.
@@ -57,10 +86,19 @@ public class Ex1 {
     public static boolean isNumber(String a) {
         boolean ans = true;
         // add your code here
-
+        int indexb = a.lastIndexOf('b');
+        String numberpart = a.substring(0, indexb);
+        char basechar = Character.toUpperCase(a.charAt(indexb + 1));
+        char[] eachnumber = numberpart.toCharArray();
+        for (int i = 0; i < eachnumber.length; i++) {
+            if (eachnumber[i] >= '0' && eachnumber[i] <= '9' && eachnumber[i] <= basechar) {
+                ans = true;
+            } else ans = false;
+        }
         ////////////////////
         return ans;
     }
+
 
     /**
      * Calculate the number representation (in basis base)
@@ -71,10 +109,19 @@ public class Ex1 {
      * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
      */
     public static String int2Number(int num, int base) {
-        String ans = "";
         // add your code here
+        String ans = "";
+        if(num > 0 && base >=2 && base <= 16) {
+            while (num >= base) {
+                int x = num / base;
+                int remainder = num % base;
+                ans = remainder + ans;
+                num = x;
+            }
+            ans = num + ans;
+        }
 
-        ////////////////////
+            ////////////////////
         return ans;
     }
 
@@ -85,8 +132,13 @@ public class Ex1 {
      * @return true iff the two numbers have the same values.
      */
     public static boolean equals(String n1, String n2) {
+        boolean ans = true;
         // add your code here
-        return n1.equals(n2);
+        if(number2Int(n1) != number2Int(n2)){
+            ans = false;
+
+        }
+        return ans;
         ////////////////////
     }
 
--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,4 +1,3 @@
-package assignments.ex1;
 import java.util.Scanner;
 
 /**
@@ -8,19 +7,24 @@ import java.util.Scanner;
  */
 public class Ex1Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String num1 = "", num2="", quit = "quit";
-        while (!num1.equals(quit) && !num2.equals(quit)) {
-            System.out.println();
-            System.out.println("Ex1 class solution:");
-            System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
-            num1 = sc.next();
-            if (!num1.equals("quit")) {
-                // add your code here
+//        Scanner sc = new Scanner(System.in);
+//        String num1 = "", num2="", quit = "quit";
+//        while (!num1.equals(quit) && !num2.equals(quit)) {
+//            System.out.println();
+//            System.out.println("Ex1 class solution:");
+//            System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
+//            num1 = sc.next();
+//            if (!num1.equals("quit")) {
+//                // add your code here
+//
+//                /////////////////////
+//            }
+//        }
+//        System.out.println("quiting now...");
 
-                /////////////////////
-            }
-        }
-        System.out.println("quiting now...");
+//        System.out.println(Ex1.number2Int("bEFbG"));;
+//        System.out.println(Ex1.isNumber("163;b2"))
+//        System.out.println(Ex1.int2Number(123,11));
+        System.out.println(Ex1.equals("5b2","101b10"));
     }
 }
